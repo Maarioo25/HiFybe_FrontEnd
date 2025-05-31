07d48e7b1ab9e20160d8c920d6149e2fdf394244
Mario Bueno <maarioo2525@gmail.com>
2025-05-31 13:33:18 +0200

    Llamada desde el front a recomendaciones

--- src/services/musicService.js
+++ src/services/musicService.js
@@ -1,28 +1,41 @@
-import api from './api';
+import axios from 'axios';
+
 export const musicService = {
-    getSongs: async filters => {
-      const res = await api.get('/canciones', { params: filters });
-      return res.data;
-    },
-    getPlaylists: async () => {
-      const res = await api.get('/playlists');
-      return res.data;
-    },
-    createPlaylist: async data => {
-      const res = await api.post('/playlists', data);
-      return res.data;
-    },
-    addSongToPlaylist: async (pid, sid) => {
-      const res = await api.post(`/playlists/${pid}/canciones`, { songId: sid });
-      return res.data;
-    },
-    getSpotifyRecommendations: async () => {
-      const token = localStorage.getItem('sp_token');
-      const res = await api.get('/spotify/recomendaciones', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      return res.data;
-    }
-  };
+  getSongs: async filters => {
+    const res = await axios.get('/canciones', { params: filters });
+    return res.data;
+  },
+  getPlaylists: async () => {
+    const res = await axios.get('/playlists');
+    return res.data;
+  },
+  createPlaylist: async data => {
+    const res = await axios.post('/playlists', data);
+    return res.data;
+  },
+  addSongToPlaylist: async (pid, sid) => {
+    const res = await axios.post(`/playlists/${pid}/canciones`, { songId: sid });
+    return res.data;
+  },
+  getSpotifyRecommendations: async () => {
+    const token = localStorage.getItem('sp_token');
+    if (!token) throw new Error("No hay token");
+
+    const res = await axios.get('https://api.spotify.com/v1/recommendations', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        seed_genres: 'pop',
+        limit: 10
+      }
+    });
+
+    return res.data.tracks.map(track => ({
+      spotifyUri: track.uri,
+      title: track.name,
+      artist: track.artists.map(a => a.name).join(', '),
+      img: track.album.images?.[0]?.url || ''
+    }));
+  }
+};
