1fd280fdad4bbd4205754fdef8186dd9fcdeb060
Mario Bueno <maarioo2525@gmail.com>
2025-06-03 16:07:23 +0200

    errores

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const originalConsoleError = console.error;
+
+console.error = (...args) => {
+  const msg = args[0];
+  if (typeof msg === 'string' && msg.includes('cpapi.spotify.com') && msg.includes('item_before_load')) {
+    return;
+  }
+  originalConsoleError(...args);
+};
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
